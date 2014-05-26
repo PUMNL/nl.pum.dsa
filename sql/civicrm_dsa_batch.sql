d15109a04b5df1e5984d9f12d4bcdacc11953b71
Ralph Kersten <helpdesk@pum.nl>
2014-05-26 12:11:21 +0200

    Removed default for importdate in civicrm_dsa_batch

--- sql/civicrm_dsa_batch.sql
+++ sql/civicrm_dsa_batch.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `civicrm_dsa_batch` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `importdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `importdate` datetime NOT NULL,
   `startdate` date DEFAULT NULL,
   `enddate` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARACTER SET=utf8 COLLATE='utf8_unicode_ci' COMMENT='nl.pum.dsa'
+) ENGINE=InnoDB  DEFAULT CHARACTER SET=utf8 COLLATE='utf8_unicode_ci' COMMENT='nl.pum.dsa';
