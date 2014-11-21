056f7b605e3ddac6d67d2a5ea5aaef035867d9fe
GitGet <gitget@pum.nl>
2014-11-21 16:14:50 +0100

    Added: API for financials

---
+++ api/v3/Representative/GetFinancials.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * Representative.GetFinancials API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+function _civicrm_api3_representative_getfinancials_spec(&$spec) {
+  $spec['contact_id'] = array(
+		'title'			=> 'Contact id',
+		'type'			=> 'integer',
+		'api.required'	=> 1,
+	);
+}
+
+/**
+ * Representative.GetFinancials API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_representative_getfinancials($params) {
+  $filtered_params = _civicrm_api3_representative_getfinancials_filter_params($params);
+	if (count($filtered_params) == 0) {
+		throw new API_Exception('No accepted parameters found', 1001);
+	}
+
+	$dao = _civicrm_api3_representative_getfinancials_dao($filtered_params);
+	$result = array();
+	while ($dao->fetch()) {
+		// collect data
+		$result[] = array(
+			'type' => $dao->type,
+			'reference' => $dao->case_sequence . ' ' . $dao->case_type . ' ' . $dao->case_country,
+			'description' => $dao->subject,
+			'payment_type' =>$dao->payment_type,
+			'amount' => $dao->total_amount,
+			'contact_id' => $dao->contact_id,
+			'date' => $dao->payment_datetime,
+		);
+	}
+    return civicrm_api3_create_success($result, $params);
+}
+
+function _civicrm_api3_representative_getfinancials_filter_params($params) {
+	$required = array(
+		'contact_id',
+	);
+	$result = array();
+	foreach($params as $key => $value) {
+		if (in_array($key, $required)) {
+			$result[$key] = $value;
+		}
+	}
+	return $result;
+}
+
+function _civicrm_api3_representative_getfinancials_dao($params) {
+	$tbl = array(
+		'Donor_details_FA'  => _getCustomTableInfo('Donor_details_FA'),	// contains donor (sponsor) code
+	);
+
+	$sql = '
+SELECT
+  \'Representative payment\' type,
+  cas.id case_id,
+  num.case_sequence,
+  num.case_type,
+  num.case_country,
+  cas.case_type_id,
+  act.id act_id,
+  act.activity_type_id,
+  act.subject,
+  act.activity_date_time,
+  IF(dsa.type = 1, \'D\', \'C\') payment_type,
+  (
+    dsa.amount_rep
+  ) AS total_amount,
+  dsa.invoice_number,
+  \'\' credited_activity_id,
+  dsa.payment_id,
+  pay.`timestamp` payment_datetime,
+  ifnull(dsa.donor_id, \'\') donor_id,
+  ifnull(dnr.display_name, \'\') donor_name,
+  ifnull(' . $tbl['Donor_details_FA']['columns']['Donor_code']['column_name'] . ', \'\') donor_code,
+  dsa.contact_id,
+  con.display_name
+FROM
+  civicrm_representative_compose dsa
+  LEFT JOIN civicrm_contact dnr
+    ON dnr.id = dsa.donor_id
+  LEFT JOIN ' . $tbl['Donor_details_FA']['group_table'] . '
+    ON ' . $tbl['Donor_details_FA']['group_table'] . '.entity_id = dnr.id
+  LEFT JOIN civicrm_dsa_payment pay
+    ON pay.id = dsa.payment_id
+  LEFT JOIN civicrm_contact con
+    ON con.id = dsa.contact_id
+  ,
+  civicrm_activity act,
+  civicrm_case_activity cac,
+  civicrm_case cas
+  LEFT JOIN civicrm_case_pum num
+    ON num.entity_id = cas.id
+WHERE
+  dsa.activity_id = ifnull(
+                      act.original_id,
+                      act.id) AND
+  act.activity_type_id IN (SELECT
+                             ovl1.value
+                           FROM
+                             civicrm_option_group ogp1,
+                             civicrm_option_value ovl1
+                           WHERE
+                             ogp1.name = \'activity_type\' AND
+                             ovl1.option_group_id = ogp1.id AND
+                             ovl1.name = \'Representative payment\') AND
+  act.status_id IN (SELECT
+                      ovl2.value
+                    FROM
+                      civicrm_option_group ogp2,
+                      civicrm_option_value ovl2
+                    WHERE
+                      ogp2.name = \'activity_status\' AND
+                      ovl2.option_group_id = ogp2.id AND
+                      ovl2.name = \'dsa_paid\') AND
+  act.is_current_revision = 1 AND
+  dsa.contact_id = ' . $params['contact_id'] . ' AND
+  cac.activity_id = act.id AND
+  cas.id = cac.case_id
+ORDER BY
+  case_sequence,
+  payment_datetime
+	';
+	
+	$dao = CRM_Core_DAO::executeQuery($sql);
+	
+	return $dao;
+}
